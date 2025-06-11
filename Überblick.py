b974494913fadce93c1fa7ea5a11a9a19e0b02cc
Martin Writz <martin.writz@ktn.gv.at>
2025-06-11 11:27:35 +0200

    .

--- Überblick.py
+++ Überblick.py
@@ -1,21 +1,35 @@
 
-import streamlit as st
-import streamlit.components.v1 as components
-from st_bridge import bridge
+#import streamlit as st
+#import streamlit.components.v1 as components
+#from st_bridge import bridge
 #from streamlit_javascript import st_javascript
 
-with open("map.html", "r", encoding="utf-8") as f:
-        bokeh_map = f.read()
+#with open("map.html", "r", encoding="utf-8") as f:
+#        bokeh_map = f.read()
 
-components.html(f"""
-            {bokeh_map}
-        """, height=300, scrolling=False)
+#components.html(f"""
+#            {bokeh_map}
+#        """, height=300, scrolling=False)
 
-data = bridge("my-bridge", default="Keine GKZs ausgewählt")
+#data = bridge("my-bridge", default="Keine GKZs ausgewählt")
 
-gkz_list = []
-if data != 'Keine GKZs ausgewählt':
-    gkz_list = [elem for elem in data["selected_gkz"] if elem != '']
+#gkz_list = []
+#if data != 'Keine GKZs ausgewählt':
+#    gkz_list = [elem for elem in data["selected_gkz"] if elem != '']
 
-st.write(gkz_list)
+#st.write(gkz_list)
 
+from st_bridge import bridge, html
+import streamlit as st
+
+data = bridge("my-bridge", default=None)
+
+# Use "window.top" in your HTML/JS
+html("""
+<button onClick="window.top.stBridges.send('my-bridge', 'clicked!')">
+  Click me
+</button>
+""", unsafe_allow_html=True)
+
+if data:
+    st.write("Bridge data received:", data)
