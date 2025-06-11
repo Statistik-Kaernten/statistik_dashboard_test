c4b35a2ac4235c396c782440ad3f4c756b6ae1b4
Landesstelle für Statistik Kärnten <156789137+Statistik-Kaernten@users.noreply.github.com>
2025-06-11 07:33:40 +0200

    Update 5_Bevoelkerung.py

--- pages/5_Bevoelkerung.py
+++ pages/5_Bevoelkerung.py
@@ -73,7 +73,7 @@ st.write('## Bevölkerung')
 
 #st.write(f'### Bevölkerungspyramide - {select_end_jahr}')
 
-data = bridge("my-bridge")
+data = bridge("my-bridge", default='Keine GKZs ausgewählt')
 st.write(data)
 gkz_list = []
 if data != 'Keine GKZs ausgewählt':
