f46efeb024f72f131c475d60026fde05523c68e3
Martin Writz <martin.writz@ktn.gv.at>
2025-06-13 11:33:26 +0200

    .

--- Überblick.py
+++ Überblick.py
@@ -1,67 +1,12 @@
-
-#import streamlit as st
-#import streamlit.components.v1 as components
-#from st_bridge import bridge
-#from streamlit_javascript import st_javascript
-
-#with open("map.html", "r", encoding="utf-8") as f:
-#        bokeh_map = f.read()
-
-#components.html(f"""
-#            {bokeh_map}
-#        """, height=300, scrolling=False)
-
-#data = bridge("my-bridge", default="Keine GKZs ausgewählt")
-
-#gkz_list = []
-#if data != 'Keine GKZs ausgewählt':
-#    gkz_list = [elem for elem in data["selected_gkz"] if elem != '']
-
-#st.write(gkz_list)
-
-#from st_bridge import bridge, html
-#import streamlit as st#
-
-#data = bridge("my-bridge", default=None)
-
-# Use "window.top" in your HTML/JS
-#html("""
-#<button onClick="window.top.stBridges.send('my-bridge', 'clicked!')">
-#  Click me
-#</button>
-#""", unsafe_allow_html=True)
-
-#if data:
-#    st.write("Bridge data received:", data)
-
-#import streamlit as st
-#import streamlit.components.v1 as components
-
-# Declare the component:
-#my_component = components.declare_component("my_component", path="frontend/build")
-
-# Use it:
-#my_component(greeting="Hello", name="World")
-
-
-import plotly.express as px
 import streamlit as st
 import pandas as pd
+import numpy as np
+from dbConnection import dbConnection
 
-from plotly1 import plotly_events
-# from plotly2 import plotly_events
-#from plotly3 import plotly_events
 
-data = {
-    'sepal_length': [5.1, 4.9, 4.7, 6.0, 6.4],
-    'sepal_width':  [3.5, 3.0, 3.2, 2.2, 3.2],
-    'petal_length': [1.4, 1.4, 1.3, 5.0, 4.5],
-    'petal_width':  [0.2, 0.2, 0.2, 1.5, 1.5],
-    'species': ['setosa', 'setosa', 'setosa', 'versicolor', 'virginica']
-}
 
-df = pd.DataFrame(data)
-fig = px.scatter(df, x="sepal_width", y="sepal_length", title="Sample Figure")
-
-value = plotly_events(fig)
-st.write("Received", value)
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df, size=5, color="#F56D8D")
