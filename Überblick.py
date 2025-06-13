9236affae9dcf735f2888ba6964ba64c47365c84
Martin Writz <martin.writz@ktn.gv.at>
2025-06-13 11:38:25 +0200

    .

--- Überblick.py
+++ Überblick.py
@@ -1,12 +1,36 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-#from dbConnection import dbConnection
+import pydeck as pdk
 
+# Sample data
+data = pd.DataFrame({
+    'lat': [37.7749, 34.0522],
+    'lon': [-122.4194, -118.2437],
+    'city': ['San Francisco', 'Los Angeles']
+})
 
-
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
+# Define the layer
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    data,
+    get_position='[lon, lat]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=10000,
+    pickable=True  # Enables hover
 )
-st.map(df, size=5, color="#F56D8D")
+
+# Define the tooltip
+tooltip = {"html": "<b>City:</b> {city}", "style": {"color": "white"}}
+
+# Create deck.gl map
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=36.5,
+        longitude=-120,
+        zoom=5,
+        pitch=50,
+    ),
+    layers=[layer],
+    tooltip=tooltip
+))
